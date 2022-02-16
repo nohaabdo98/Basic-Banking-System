a0de8a0e2affaf528995a2a8672749e23fffbadd
nohaabdo98 <62175401+nohaabdo98@users.noreply.github.com>
2022-02-16 15:39:55 +0200

    update-bank-system

--- All_Customers.php
+++ All_Customers.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Customers | Bank System</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/table.css">
 
     <style type="text/css">
       button{
